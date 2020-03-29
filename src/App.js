186db07e5739d08e0a21d763f76937f685a7afe8
Christopher MacDonald <cmacdonald131@gmail.com>
2020-03-29 07:42:34 -0400

    add components

--- src/App.js
+++ src/App.js
@@ -1,20 +1,73 @@
 import React, { Component } from 'react'
-import { Route } from 'react-router-dom'
+import { Route, Switch } from 'react-router-dom'
 import Landing from '../src/Routes/Landing/Landing'
 import LoginForm from '../src/Routes/LoginForm/LoginForm'
 import SignupForm from './Routes/SignupForm/SignupForm'
 import SearchSection from '../src/SearchFields/SearchSection/SearchSection'
+import TokenService from './Services/token-service'
+import IdleService from './Services/idle-service'
+import AuthApiService from './Services/auth-api-service'
+import ApiContext from './ApiContext'
+
 
 
 class App extends Component {
+  state = {
+    hasError: false,
+    user: TokenService.getAuthToken(),
+  }
+
+  static getDerivedStateFromError(error) {
+    console.error(error)
+    return { hasError: true }
+  }
+
+  componentDidMount() {
+    IdleService.setIdleCallback(this.logoutFromIdle)
+
+    if (TokenService.hasAuthToken()) {
+      IdleService.regiserIdleTimerResets()
+      TokenService.queueCallbackBeforeExpiry(() => {
+        AuthApiService.postRefreshToken()
+      })
+    }
+  }
+
+  setUser = (user) => {
+    this.setState({
+      user
+    })
+  }
+
+  componentWillUnmount() {
+    IdleService.unRegisterIdleResets()
+    TokenService.clearCallbackBeforeExpiry()
+  }
+
+  logoutFromIdle = () => {
+    TokenService.clearAuthToken()
+    TokenService.clearCallbackBeforeExpiry()
+    IdleService.unRegisterIdleResets()
+
+    this.forceUpdate()
+  }
+
+
   render() {
     return (
-      <div className="flightType">
-        <Route exact path="/" component={SearchSection} />
-        <Route path="/about" component={Landing} />
-        <Route path="/login" component={LoginForm} />
-        <Route path="/signup" component={SignupForm} />
-      </div>
+      <ApiContext.Provider value={{
+        user: this.state.user,
+        setUser: this.setUser,
+      }}>
+        <div className="flightType">
+          <Switch>
+            <Route exact path="/" component={SearchSection} />
+            <Route path="/about" component={Landing} />
+            <Route path="/login" component={LoginForm} />
+            <Route path="/signup" component={SignupForm} />
+          </Switch>
+        </div>
+      </ApiContext.Provider>
     )
   }
 }
